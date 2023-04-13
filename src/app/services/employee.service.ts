b44d54cca3f94c243562a020143454be1a65e101
anaserror404 <anasnaciridev@gmail.com>
2023-04-13 23:21:03 +0000

    AddPlayer

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  constructor(private _http: HttpClient) {}
+
+  addEmployee(data: any): Observable<any> {
+    return this._http.post('http://localhost:8080/users', data);
+  }
+
+  updateEmployee(id: string, data: any): Observable<any> {
+    return this._http.put(`http://localhost:8080/users/${id}`, data);
+  }
+
+  getEmployeeList(): Observable<any> {
+    return this._http.get('http://localhost:8080/users');
+  }
+
+  deleteEmployee(id: string): Observable<any> {
+    return this._http.delete(`http://localhost:8080/users/${id}`);
+  }
+}
